5a4c5e3cf2757bf165533866a06e9b3f2da48e5d
Elena N.J. <elena.nicole.jacobsen@hotmail.com>
2025-02-12 02:02:03 +0100

    First iteration of MessageUtils.java
    Finished class. Added methods for encapsulation and decapsulation. Added simple comments for better readability.

--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -1,48 +1,35 @@
 package no.hvl.dat110.messaging;
 
 import java.util.Arrays;
-
 import no.hvl.dat110.TODO;
 
 public class MessageUtils {
 
+	// Constants:
 	public static final int SEGMENTSIZE = 128;
 
+	// Variables:
 	public static int MESSAGINGPORT = 8080;
 	public static String MESSAGINGHOST = "localhost";
 
+	// Method for encapsulation:
 	public static byte[] encapsulate(Message message) {
+		byte[] segment = new byte[SEGMENTSIZE];
+		byte[] data = message.getData();
 		
-		byte[] segment = null;
-		byte[] data;
-		
-		// TODO - START
-		
-		// encapulate/encode the payload data of the message and form a segment
-		// according to the segment format for the messaging layer
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
-		// TODO - END
+		segment[0] = (byte)data.length;
+		System.arraycopy(data, 0, segment, 1, data.length);
+
 		return segment;
-		
 	}
 
+	// Method for decapsulation:
 	public static Message decapsulate(byte[] segment) {
+		byte length = segment[0];
+		byte[] data = new byte[length];
+		
+		System.arraycopy(segment, 1, data, 0, data.length);
 
-		Message message = null;
-		
-		// TODO - START
-		// decapsulate segment and put received payload data into a message
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-		return message;
-		
+        return new Message(data);
 	}
-	
 }
