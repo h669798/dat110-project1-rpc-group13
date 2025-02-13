48413ea5a33a148b734218ceb24e921d210e8451
SarahSAH02 <674293@stud.hvl.no>
2025-02-13 23:24:10 +0100

    controll done

--- src/main/java/no/hvl/dat110/system/controller/Controller.java
+++ src/main/java/no/hvl/dat110/system/controller/Controller.java
@@ -1,6 +1,6 @@
 package no.hvl.dat110.system.controller;
 
-import no.hvl.dat110.TODO;
+
 import no.hvl.dat110.rpc.RPCClient;
 import no.hvl.dat110.rpc.RPCClientStopStub;
 
@@ -31,9 +31,17 @@ public class Controller  {
 		// connect to sensor and display RPC servers - using the RPCClients
 		// read value from sensor using RPC and write to display using RPC
 			
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
+		display = new DisplayStub(displayclient);
+		sensor = new SensorStub(sensorclient);
+
+		displayclient.connect();
+		sensorclient.connect();
+
+		// finite loop
+		for (int i = 0; i < 10; i++) {
+			 int temp = (int) Math.round(sensor.read());  // Avrund verdien  
+			    display.write(String.valueOf(temp)); 
+		}
 		// TODO - END
 		
 		stopdisplay.stop();
--- src/main/java/no/hvl/dat110/system/controller/DisplayStub.java
+++ src/main/java/no/hvl/dat110/system/controller/DisplayStub.java
@@ -1,6 +1,5 @@
 package no.hvl.dat110.system.controller;
 
-import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.*;
 
 public class DisplayStub extends RPCLocalStub {
@@ -15,8 +14,13 @@ public class DisplayStub extends RPCLocalStub {
 		
 		// implement marshalling, call and unmarshalling for write RPC method
 		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
+		
+		byte[] data = RPCUtils.marshallString(message);
+		
+		byte[] response = rpcclient.call((byte)Common.WRITE_RPCID,data);
+		
+	 RPCUtils.unmarshallString(response);
+		
 		
 		// TODO - END
 		
